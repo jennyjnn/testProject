a2f1402c836c1d9ddd467fc964b5ce83d43efe34
DESKTOP-8IVL5NV\Jenny <58070018@kmitl.ac.th>
2018-07-18 12:22:08 +0700

    TextView

--- practice1/app/src/main/java/com/example/jenny/practice1/MainActivity.java
+++ practice1/app/src/main/java/com/example/jenny/practice1/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.jenny.practice1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView tvHello = findViewById(R.id.tvHello);
+        tvHello.setMovementMethod(LinkMovementMethod.getInstance());
+        tvHello.setText(Html.fromHtml("<b>He<u>ll</u>o</b> <i>World</i> <a href=\"https://www.google.com/\">Google</a>"));
     }
 }
