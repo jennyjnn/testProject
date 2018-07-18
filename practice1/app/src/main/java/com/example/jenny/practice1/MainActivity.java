ae92dfbaae1c6dafe59e275dcec4cf8cc1a019f6
DESKTOP-8IVL5NV\Jenny <58070018@kmitl.ac.th>
2018-07-18 12:57:36 +0700

    EditText

--- practice1/app/src/main/java/com/example/jenny/practice1/MainActivity.java
+++ practice1/app/src/main/java/com/example/jenny/practice1/MainActivity.java
@@ -4,6 +4,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Html;
 import android.text.method.LinkMovementMethod;
+import android.view.KeyEvent;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,8 +16,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView tvHello = findViewById(R.id.tvHello);
+        final TextView tvHello = findViewById(R.id.tvHello);
         tvHello.setMovementMethod(LinkMovementMethod.getInstance());
         tvHello.setText(Html.fromHtml("<b>He<u>ll</u>o</b> <i>World</i> <a href=\"https://www.google.com/\">Google</a>"));
+
+        final EditText editTextHello = findViewById(R.id.editTextHello);
+        editTextHello.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_DONE){
+                    // Copy text in EditText to TextView
+                    tvHello.setText(editTextHello.getText());
+                    return true;
+                }
+                return false;
+            }
+        });
     }
 }
