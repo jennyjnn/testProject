0fc22236c2de30ca1a7d3b06934916912391218e
LAPTOP-3V7FAR52\Jenny <58070018@kmitl.ac.th>
2018-08-23 23:42:50 +0700

    Custom View

---
+++ practice1/app/src/main/java/com/example/jenny/practice1/CustomView.java
@@ -0,0 +1,46 @@
+package com.example.jenny.practice1;
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.os.Build;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class CustomView extends View {
+    public CustomView(Context context) {
+        super(context);
+        init();
+    }
+
+    public CustomView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public CustomView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    @TargetApi(21)
+    public CustomView(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        init();
+    }
+
+    private void init() {
+
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        Paint p = new Paint();
+        p.setColor(0xFFFF0000); //AARRGGBB
+        canvas.drawLine(0, 0, getMeasuredWidth(), getMeasuredHeight(), p);
+    }
+}
