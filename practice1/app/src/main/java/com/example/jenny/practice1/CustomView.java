5e31030f7b7c4bad97919083d0c6fc360439044c
LAPTOP-3V7FAR52\Jenny <58070018@kmitl.ac.th>
2018-08-26 17:45:51 +0700

    Custom Attribute

--- practice1/app/src/main/java/com/example/jenny/practice1/CustomView.java
+++ practice1/app/src/main/java/com/example/jenny/practice1/CustomView.java
@@ -2,6 +2,7 @@ package com.example.jenny.practice1;
 
 import android.annotation.TargetApi;
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.os.Build;
@@ -10,6 +11,8 @@ import android.util.AttributeSet;
 import android.view.View;
 
 public class CustomView extends View {
+    private boolean isBlue = false;
+
     public CustomView(Context context) {
         super(context);
         init();
@@ -18,21 +21,36 @@ public class CustomView extends View {
     public CustomView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
         init();
+        initWithAttrs(attrs, 0, 0);
     }
 
     public CustomView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
         init();
+        initWithAttrs(attrs, defStyleAttr, 0);
     }
 
     @TargetApi(21)
     public CustomView(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
         super(context, attrs, defStyleAttr, defStyleRes);
         init();
+        initWithAttrs(attrs, defStyleAttr, defStyleRes);
     }
 
     private void init() {
 
+    }
+
+    private void initWithAttrs(AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        TypedArray a = getContext().getTheme().obtainStyledAttributes(
+                attrs,
+                R.styleable.CustomView,
+                defStyleAttr, defStyleRes);
+        try {
+            isBlue = a.getBoolean(R.styleable.CustomView_isBlue, false);
+        } finally {
+            a.recycle();
+        }
     }
 
     @Override
@@ -40,7 +58,11 @@ public class CustomView extends View {
         super.onDraw(canvas);
 
         Paint p = new Paint();
-        p.setColor(0xFFFF0000); //AARRGGBB
+
+        if (isBlue)
+            p.setColor(0xFF0000FF); //AARRGGBB
+        else
+            p.setColor(0xFFFF0000); //AARRGGBB
         canvas.drawLine(0, 0, getMeasuredWidth(), getMeasuredHeight(), p);
     }
 }
